8dbb6d6bb790f2b5134ff2ed8668a990f77fe295
Milan Topuzov <topuzov.work@gmail.com>
2015-08-21 16:17:19 +0200

    added models and admin

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from data.models import Keyword, Result
+
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+
+    pass
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+
+    list_display = ('keyword', 'url', 'position')
--- data/models.py
+++ data/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+
+    keyword = models.CharField(max_length=100, unique=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.keyword
+
+
+class Result(models.Model):
+
+    keyword = models.ForeignKey('Keyword')
+    url = models.URLField()
+    position = models.IntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
